c000215330e58c4d66736c6be111bfc74388fcec
David masterson <david73180@gmail.com>
2018-10-16 05:22:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist
+
+  attr_accessor :name, :song
+
+  def initialize
+    @name = name
+  end
+
+  def song
+    @song = Artist.new
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author
+
+  attr_accessor :name, :post
+
+  def initialize
+    @name = name
+  end
+
+  def post
+    @post = Author.new
+  end
+
+end
