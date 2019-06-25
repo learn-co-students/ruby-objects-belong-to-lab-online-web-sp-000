76f09809621195a0045e5a39f974d0a500ac17ba
Frederic Silber <freddy.silber@cloudcoach.com>
2019-06-25 23:12:03 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,16 @@
+class Author
+
+attr_accessor :name
+
+def initialize(name = nil)
+
+  @name = name
+end
+
+
+def name
+  @name
+end
+
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+
+attr_accessor :title, :author
+
+def initialize(title = nil)
+  @title = title
+end
+
+def title
+  @title
+end
+
+
+end
