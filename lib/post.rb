7a0f3fd97e8fe7cec2b775fab15df5ef3bd35b9a
Peter Grant <jgrantps@hotmail.com>
2019-07-03 18:36:36 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+  attr_accessor :title, #:author
+  def initialize(title)
+    @title = title
+
+  end
+
+ def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+
+
+end
