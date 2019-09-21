515645a2c8f861df63b72877d8e75eac959b6add
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-09-21 17:01:32 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,22 @@
+class Author
+  
+  # attr_accessor :name, :post
+  
+  def name= (name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def post= (post)
+    @post = post
+  end
+  
+  def post
+    @post
+  end
+  
+  
+end
