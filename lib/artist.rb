77c42e0ce96689da1e4c38997a6086af3342f3fc
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-23 20:42:46 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,11 @@
 class Artist
   attr_accessor :name
 
-  def initialize(name)
+  def initialize(name = "default")
     @name = name
     @songs = []
   end
-  
+
   def add_song(song)
   @songs << song
   song.artist = self
