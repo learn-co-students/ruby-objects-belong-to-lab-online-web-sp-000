c19e1a9cb18e098a8c85e9733001319ce6018355
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-25 16:17:49 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def initilaize
+    @title = title
+    @author = author.name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def initilaize
+    @title = title
+    @artist - artist.name
+  end
+
+end
