74d0755f21feaf3bbf8d6fd827f7c0a4437fac7f
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-01 02:05:25 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Song  
+  attr_accessor :title, :artist 
+
+def initialize
+  @title = title 
+end 
+end 
+
+
+class Artist 
+  attr_accessor :name, 
+
+def initialize 
+  @name = name
+end 
+end 
+
+class Author 
+  attr_accessor :name, :author  
+  
+  def initialize
+    @name = name 
+  end 
+end 
+
+class Post 
+  attr_accessor :title 
+
+  def initialize
+    @name = name 
+  end 
+end 
+
+
+  
