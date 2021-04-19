4dd05375410e12442e0efdebffe7b25e61aac37a
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-19 20:25:03 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,6 @@
+class Artist
+attr_accessor :name
+  def Artist(name)
+    @name = name
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,6 @@
+class Author
+attr_accessor :name
+  def Author(name)
+    @name = name
+  end
+end
